5fcf6991796c822ed33aa6581799d5a3ea062f09
Nicolas Neirynck <nicolas.neirynck2@student.hogent.be>
2025-12-10 15:30:38 +0100

    ci/cd backend

--- fridgepal-api/src/drizzle/drizzle.module.ts
+++ fridgepal-api/src/drizzle/drizzle.module.ts
@@ -1,19 +1,25 @@
-import { Module, OnModuleDestroy } from '@nestjs/common';
+import { Module, OnModuleDestroy, Logger } from '@nestjs/common';
 import {
   type DatabaseProvider,
   DrizzleAsyncProvider,
   drizzleProvider,
   InjectDrizzle,
 } from './drizzle.provider';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+import path from 'node:path';
 
 @Module({
   providers: [...drizzleProvider],
   exports: [DrizzleAsyncProvider],
 })
 export class DrizzleModule implements OnModuleDestroy {
+  private readonly logger = new Logger(DrizzleModule.name);
   constructor(@InjectDrizzle() private readonly db: DatabaseProvider) {}
-
-  // 👇 2
+  async onModuleInit() {
+    this.logger.log('Running migrations..');
+    await migrate(this.db, { migrationsFolder: path.resolve('migrations') });
+    this.logger.log('Migrations completed');
+  }
   async onModuleDestroy() {
     await this.db.$client.end();
   }
