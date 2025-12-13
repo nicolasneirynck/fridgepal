ed9cd96fd519ad8f6ef0fe3cd9eb5b214c80210c
Nicolas Neirynck <nicolas.neirynck2@student.hogent.be>
2025-12-13 09:22:07 +0100

    detail

--- fridgepal-frontend/src/components/search-recipe/SelectedIngredients.jsx
+++ fridgepal-frontend/src/components/search-recipe/SelectedIngredients.jsx
@@ -5,14 +5,14 @@ export default function IngredientList({ ingredients = [],handleDeleteIngredient
   if (ingredients.length === 0) {
     return (
       <p className="text-gray-400 text-sm italic mt-3 mb-2">
-        No ingredients added yet
+        Nog geen ingrediënten toegevoegd
       </p>
     );
   }
 
   return (
     <div className="mt-3 mb-2">
-      <p className="text-gray-500 text-sm font-medium">Your ingredients:</p>
+      <p className="text-gray-500 text-sm font-medium">Jouw ingrediënten:</p>
       <div className="flex gap-2 flex-wrap">
         {ingredients.map((ingredient) => (
           <IngredientButton key={ingredient} 
